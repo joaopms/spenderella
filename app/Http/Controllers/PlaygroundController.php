e09ff5d574c9d2e65fbe8f00d753ae4fa8cbf61d
João Silva <joaopms@users.noreply.github.com>
2023-09-22 01:35:17 +0100

    Refactor the NordigenClient auxiliary functions into a new class

--- app/Http/Controllers/PlaygroundController.php
+++ app/Http/Controllers/PlaygroundController.php
@@ -2,30 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Integrations\Nordigen\NordigenClient;
 use App\Models\NordigenAgreement;
 use App\Models\NordigenRequisition;
 use Exception;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Nordigen\NordigenPHP\API\NordigenClient;
 
 class PlaygroundController extends Controller
 {
-    const SANDBOX_INSTITUTION = 'SANDBOXFINANCE_SFIN0000';
-
     const REQUISITION_STATUS_LINKED = 'LN';
-
-    const NORDIGEN_ACCESS_TOKEN = 'nordigen.access_token';
-
-    const NORDIGEN_REFRESH_TOKEN = 'nordigen.refresh_token';
 
     public function createRequisition()
     {
-        $client = $this->getNordigenClient();
+        $client = new NordigenClient();
 
-        $institutionId = self::SANDBOX_INSTITUTION;
+        $institutionId = NordigenClient::SANDBOX_INSTITUTION;
 
         // Create the end user agreement
         $agreementData = $client->endUserAgreement->createEndUserAgreement($institutionId);
@@ -72,7 +65,7 @@ class PlaygroundController extends Controller
             return response()->noContent();
         }
 
-        $client = $this->getNordigenClient();
+        $client = new NordigenClient();
 
         // Get the requisition data from Nordigen
         $requisitionData = $client->requisition->getRequisition($requisition->nordigen_id);
@@ -117,55 +110,5 @@ class PlaygroundController extends Controller
         $requisition->load('accounts');
 
         return response($requisition, Response::HTTP_CREATED);
-    }
-
-    public function getNordigenClient(): NordigenClient
-    {
-        $client = new NordigenClient(config('services.nordigen.id'), config('services.nordigen.key'));
-
-        // Use the cached access token, if available
-        if (Cache::has(self::NORDIGEN_ACCESS_TOKEN)) {
-            Log::debug('Nordigen access token: HIT');
-            $accessToken = Cache::get(self::NORDIGEN_ACCESS_TOKEN);
-            $client->setAccessToken($accessToken);
-
-            return $client;
-        }
-
-        Log::debug('Nordigen access token: MISS');
-
-        // If the access token is not available, try to use the refresh token to get a new one
-        if (Cache::has(self::NORDIGEN_REFRESH_TOKEN)) {
-            $refreshToken = Cache::get(self::NORDIGEN_REFRESH_TOKEN);
-            Log::debug('Nordigen refresh token: HIT');
-            $response = $client->refreshAccessToken($refreshToken);
-
-            // Cache the new access token
-            $accessToken = $response['access'];
-            $accessExpires = $response['access_expires'] - 1000;
-            Cache::set(self::NORDIGEN_ACCESS_TOKEN, $accessToken, $accessExpires);
-            Log::debug("Nordigen access token: SET $accessExpires");
-
-            return $client;
-        }
-
-        Log::debug('Nordigen refresh token: MISS');
-
-        // If no refresh token is available, create a brand-new access token and cache it
-        $response = $client->createAccessToken();
-
-        // Cache the new access token
-        $accessToken = $response['access'];
-        $accessExpires = $response['access_expires'] - 1000;
-        Cache::set(self::NORDIGEN_ACCESS_TOKEN, $accessToken, $accessExpires);
-        Log::debug("Nordigen access token: SET $accessExpires");
-
-        // Cache the new response token
-        $refreshToken = $response['refresh'];
-        $refreshExpires = $response['refresh_expires'] - 1000;
-        Cache::set(self::NORDIGEN_REFRESH_TOKEN, $refreshToken, $refreshExpires);
-        Log::debug("Nordigen refresh token: SET $refreshExpires");
-
-        return $client;
     }
 }
---
+++ app/Integrations/Nordigen/NordigenClient.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Integrations\Nordigen;
+
+use GuzzleHttp\ClientInterface;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+
+class NordigenClient extends \Nordigen\NordigenPHP\API\NordigenClient
+{
+    public const SANDBOX_INSTITUTION = 'SANDBOXFINANCE_SFIN0000';
+
+    private const NORDIGEN_ACCESS_TOKEN = 'nordigen.access_token';
+
+    private const NORDIGEN_REFRESH_TOKEN = 'nordigen.refresh_token';
+
+    /**
+     * Initialize the client with values from the config and loads it with an access token
+     */
+    public function __construct(string $secretId = null, string $secretKey = null, ClientInterface $client = null)
+    {
+        parent::__construct(
+            $secretId ?? config('services.nordigen.id'),
+            $secretKey ?? config('services.nordigen.key'),
+            $client
+        );
+
+        $this->initializeClient();
+    }
+
+    private function initializeClient(): void
+    {
+        $this->prepareAccessToken();
+    }
+
+    /**
+     * Loads the access token from cache, renewing (or creating) it if necessary
+     */
+    private function prepareAccessToken(): void
+    {
+        // Use the cached access token, if available
+        if (Cache::has(self::NORDIGEN_ACCESS_TOKEN)) {
+            Log::debug('Nordigen access token: HIT');
+            $accessToken = Cache::get(self::NORDIGEN_ACCESS_TOKEN);
+            $this->setAccessToken($accessToken);
+
+            return;
+        }
+
+        Log::debug('Nordigen access token: MISS');
+
+        // If the access token is not available, try to use the refresh token to get a new one
+        if (Cache::has(self::NORDIGEN_REFRESH_TOKEN)) {
+            $refreshToken = Cache::get(self::NORDIGEN_REFRESH_TOKEN);
+            Log::debug('Nordigen refresh token: HIT');
+            $response = $this->refreshAccessToken($refreshToken);
+
+            // Cache the new access token
+            $this->cacheNewAccessToken($response);
+
+            return;
+        }
+
+        Log::debug('Nordigen refresh token: MISS');
+
+        // If no refresh token is available, create a brand-new access token and cache it
+        $response = $this->createAccessToken();
+
+        // Cache the new access token
+        $this->cacheNewAccessToken($response);
+
+        // Cache the new response token
+        $this->cacheNewResponseToken($response);
+    }
+
+    /**
+     * Caches the access token from a Nordigen token response
+     */
+    private function cacheNewAccessToken(array $response): void
+    {
+        $accessToken = $response['access'];
+        $accessExpires = $response['access_expires'] - 1000;
+        Cache::set(self::NORDIGEN_ACCESS_TOKEN, $accessToken, $accessExpires);
+        Log::debug("Nordigen access token: SET $accessExpires");
+    }
+
+    /**
+     * Caches the response token from a Nordigen token response
+     */
+    private function cacheNewResponseToken(array $response): void
+    {
+        $refreshToken = $response['refresh'];
+        $refreshExpires = $response['refresh_expires'] - 1000;
+        Cache::set(self::NORDIGEN_REFRESH_TOKEN, $refreshToken, $refreshExpires);
+        Log::debug("Nordigen refresh token: SET $refreshExpires");
+    }
+}
