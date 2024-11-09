e1fec2759d54a462515e4c23727950320843aefb
João Silva <git@joaopms.pt>
2024-11-09 22:56:11 +0000

    Use MariaDB on production

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,8 +11,4 @@ fi
 chown -R :www-data /var/www/storage
 chmod -R 775 /var/www/storage
 
-# SQLite needs to have permission to write to the folder as well, even if it only uses database.sqlite
-chown -R :www-data /var/www/database
-chmod -R 775 /var/www/database
-
 exec /usr/bin/supervisord -c /etc/supervisord.conf
