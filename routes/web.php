ee1e3f66383566d7b6387492bdb5c3aaaecc9770
João Silva <git@joaopms.pt>
2025-05-21 16:33:12 +0100

    Migrate to Laravel 11 - Part 2
    Follow the new app/directory structure

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\TransactionsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/settings', [SettingsController::class, 'show'])->name('settings.show');
 Route::post('/settings/payment-method', [SettingsController::class, 'storePaymentMethod']);
 
